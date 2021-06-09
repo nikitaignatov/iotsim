c5ac4873e9641699c40e8acc6706f68198133273
Nikita Ignatov <4703715+nikitaignatov@users.noreply.github.com>
2021-06-09 22:10:43 +0200

    localStore formatting

--- src/routes/localStore.ts
+++ src/routes/localStore.ts
@@ -2,17 +2,17 @@ import { writable } from 'svelte/store'
 import type { Writable } from 'svelte/store'
 
 export function localStore(key: string, value: string): Writable<string> {
-  const data = typeof localStorage != 'undefined' ? localStorage.getItem(key) : null
-  const store = writable(value)
-  if (data !== null) {
-    store.set(data)
-  }
-  store.subscribe(val => {
-    if (typeof localStorage == 'undefined') {
-      return
+    const data = typeof localStorage != 'undefined' ? localStorage.getItem(key) : null
+    const store = writable(value)
+    if (data !== null) {
+        store.set(data)
     }
-    localStorage.setItem(key, val)
-  })
+    store.subscribe(val => {
+        if (typeof localStorage == 'undefined') {
+            return
+        }
+        localStorage.setItem(key, val)
+    })
 
-  return store
+    return store
 }
